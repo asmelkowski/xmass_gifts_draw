3e5faffae11ff45fe5c0badfb9a441a11e5b6832
Adam Smelkowski <adamsmelkowski@gmail.com>
2019-12-06 07:29:38 +0100

    Almost done?

--- draw1.py
+++ draw1.py
@@ -1,4 +1,14 @@
 import random
+import os
+
+latest_draw = {}
+
+if os.path.exists('draw_history.txt'):
+    with open('draw_history.txt') as file:
+        for line in file.readlines():
+            line = line.split(':')
+            latest_draw[line[0]] = line[1].rstrip('\n')
+    print(latest_draw)
 
 persons = {
         'Mirosława': 'Cich',
@@ -15,28 +25,31 @@ persons = {
         'Piotr': 'Przy',
     }
 
+def draw(persons):
+    gifts_map = {x: '' for x in persons.keys()}
+    receivers = [x for x in persons.keys()]
+    for giver in gifts_map.keys():
+        loop_count = 0
+        while len(gifts_map[giver]) == 0:
+            if loop_count < 3:
+                person = random.choice(receivers)
+                breakpoint()
+                if giver != person and persons[giver] != persons[person]:
+                    if os.path.exists('draw_history.txt'):
+                        if person not in latest_draw[giver]:
+                            gifts_map[giver] = person
+                            receivers.remove(person)
+                    else:
+                        gifts_map[giver] = person
+                        receivers.remove(person)
+                else:
+                    loop_count += 1
+                    breakpoint()
+            else:
+                draw(persons)
+    with open('draw_history.txt', 'w') as history:
+        for giver, receiver in gifts_map.items():
+            history.write(f"{giver}:{receiver}\n")
+    return gifts_map
 
-givers = ['Mirosława', 'Tadeusz', 'Anna', 'Paweł', 'Sylwia',
-          'Rudi', 'Eliza', 'Dawid', 'Ola', 'Piotr', 'Adam', 'Ewa']
-receivers = givers[:]
-
-def pair(giver, receivers, persons):
-    receiver = random.choice(receivers)
-    if persons[giver] != persons[receiver] and giver != receiver:
-        return [giver, receiver]
-    else:
-        return None
-draw = []
-for giver in givers:
-    loop_count = 0
-    prop_pair = pair(giver, receivers, persons)
-    if prop_pair is not None:
-        draw.append(prop_pair)
-    else:
-        if loop_count < 5:
-            loop_count += 1
-            continue
-        else:
-            break
-print(draw)
-print(len(draw))
+print(draw(persons))
